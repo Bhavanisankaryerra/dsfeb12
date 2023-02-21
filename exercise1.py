e70ce2fc10313bffed9ff1d7354cf9d1a7ddafda
Bhavanisankaryerra <110294012+Bhavanisankaryerra@users.noreply.github.com>
2023-02-21 14:52:46 +0530

    Add files via upload

---
+++ exercise1.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Print function
+
+# Print() function in python
+# 
+# print can allow the empty paranthesis.so,print() will produce an invisible new line character which will cause a blank line to appear on your screen.
+
+# In[23]:
+
+
+print()
+
+
+# In[3]:
+
+
+print("Hello World!")
+
+
+# In[2]:
+
+
+print("hi")
+
+
+# In[13]:
+
+
+help(print)
+
+
+# syntax: print(value, ..., sep=' ', end='\n', file=sys.stdout, flush=False)
+
+# In[27]:
+
+
+print("Hello")
+print("Mr.")
+
+
+# In[26]:
+
+
+# use end
+print("Hello",end=' ')
+print("Mr.")
+
+
+# In[24]:
+
+
+print("hello","jupyter",1,2.0)
+
+
+# In[25]:
+
+
+#use sep
+print("hello","jupyter",1,2.0,sep=',')
+
+
+# string in single quote(' ') or double quote(" ") allowed in python.Both are valid.
+
+# In[28]:
+
+
+print("Welcome to python tutorial")
+
+
+# In[29]:
+
+
+print('python is easy to learn')
+
+
+# In[32]:
+
+
+print('jenny said "it is open source programming language"')
+
+
+# String concatenation
+
+# In[38]:
+
+
+print('Hello')
+print('world')
+print('Hello\nworld')
+print('hello',end=' ')
+print('world')
+
+
+# In[ ]:
+
+
+
+
